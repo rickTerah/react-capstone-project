c12b57c4a53e4792f136d850d6f1d661bbeade51
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-29 15:00:57 +0300

    protected links and routes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,17 +3,25 @@ import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 import './Navbar.scss';
 
-const navbar = () => (
+const navbar = ({ user }) => (
   <div className="navbar">
     <Link to="/" className="navbar__list-link">
       <img className="navbar__logo-img" src={logo} alt="Logo" />
     </Link>
     <ul className="navbar__list">
       <li className="navbar__list-item"><NavLink to="/" className="navbar__list-link">Home</NavLink></li>
-      <li className="navbar__list-item"><NavLink to="/login" className="navbar__list-link">Sign in</NavLink></li>
-      <li className="navbar__list-item"><NavLink to="/register" className="navbar__list-link">Sign up</NavLink></li>
-      <li className="navbar__list-item"><NavLink to="/new-article" className="navbar__list-link">New Article</NavLink></li>
-      <li className="navbar__list-item"><NavLink to="/new-gif" className="navbar__list-link">New Gif</NavLink></li>
+      {!user && (
+        <li className="navbar__list-item"><NavLink to="/login" className="navbar__list-link">Sign in</NavLink></li>
+      )}
+      {user && (
+       <React.Fragment>
+         {user.isAdmin && (
+         <li className="navbar__list-item"><NavLink to="/register" className="navbar__list-link">Register</NavLink></li>
+      )}
+          <li className="navbar__list-item"><NavLink to="/profile" className="navbar__list-link">{user.email}</NavLink></li>
+          <li className="navbar__list-item"><NavLink to="/logout" className="navbar__list-link">Logout</NavLink></li>
+       </React.Fragment>
+      )}
     </ul>
   </div>
 );
