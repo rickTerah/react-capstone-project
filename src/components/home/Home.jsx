735d82f40538d4f533d7d1b5ed597fe305818287
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-29 15:00:33 +0300

    adding home component file

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home.scss';
+
+const home = (props) => (
+    <div className="home-panel">
+        <div className="home-panel__company">
+            <h2 className="home-panel__company-name">
+                Andela
+            </h2>
+            <h3 className="home-panel__moto">
+                A place to share your knowledge and fun in life.
+            </h3>
+        </div>
+    </div>
+)
+ 
+export default home;
